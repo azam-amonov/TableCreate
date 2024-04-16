0a3068b7f2bb9f76e6385f2cb817c93407006d2c
Azam Amonov <azamamonov555@gmail.com>
2024-04-16 14:57:04 +0500

    Add Save Button

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -23,7 +23,8 @@ export class Home extends Component {
                                     <td><input type = 'text' value = {value.name} onChange ={(e) => {value.updateValue(e, 'title')}} /></td> 
                                     <td><input type = 'text' value = {value.info} onChange ={(e) => {value.updateValue(e, 'info')}} /></td> 
                                     <td><input type = 'text' value = {value.price} onChange ={(e) => {value.updateValue(e, 'price')}} /></td> 
-                                    <td><input type = 'text' value = {value.company} onChange ={(e) => {value.updateValue(e, 'company')}} /></td> 
+                                    <td><input type = 'text' value = {value.company} onChange ={(e) => {value.updateValue(e, 'company')}} /></td>
+                                    <td><Button size={'sm'} onClick={() => {value.onSave(value.id)}}> {value.id ? 'Save' : 'Add new Row'} </Button> </td>
                                 </tr>
                                 {value.AllData.map((product, index) => {
                                     return (
